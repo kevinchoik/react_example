7f8c7f696ef839ab386a7acfc0d8c795dfc24cba
Kevin Choi <kevin.choi@yale.edu>
2019-07-18 14:50:27 -0400

    Add input handler

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -3,17 +3,25 @@ import React from 'react';
 class InputLine extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			typedText: ''
+		}
 	}
 
-	clickHandler(event) {
+	submitHandler(event) {
 		event.preventDefault();
-		this.props.submit('Test Task');
+		this.props.submit(this.state.typedText);
+		this.setState({ typedText: '' });
+	}
+
+	typeHandler(event) {
+		this.setState({ typedText: event.target.value });
 	}
 
 	render() {
 		return (<form>
-			<input className='form-control inline todo-input' type='text' placeholder='To do...'/>
-			<button className='btn btn-primary inline' onClick={event => this.clickHandler(event)}>Add To Do</button>
+			<input className='form-control inline todo-input' type='text' placeholder='To do...' onChange={event => this.typeHandler(event)} value={this.state.typedText}/>
+			<button className='btn btn-primary inline' onClick={event => this.submitHandler(event)}>Add To Do</button>
 		</form>);
 	}
 }
