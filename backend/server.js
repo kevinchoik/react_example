e12e01a8d97a68a4b8f72c38269a1c777aaefc5c
Kevin Choi <kevin.choi@yale.edu>
2019-07-18 15:35:54 -0400

    Add connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const dbRoutes = require('./routes/databaseAccess.js');
+
+if (!process.env.MONGODB_URI) {
+	console.log('Source .env to add variables');
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true
+});
 
 // Make build folder publicly available
 app.use(express.static('build'));
